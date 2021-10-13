583d5c166463f90ee5fa2d642ef83ed5a3f4aeb5
anjirajardula <anjirajardula@gmail.com>
2021-10-13 09:08:26 +0530

    Added: set up the cards

--- src/main/java/com/bridgelabz/workshoptwo/DeckOfCardsMain.java
+++ src/main/java/com/bridgelabz/workshoptwo/DeckOfCardsMain.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.workshoptwo;
 
+/**
+ * Work shop on Deck of Cards Game
+ *
+ * @author : Anji Raj
+ * @version : 16.0
+ * @since : 13/10/2021
+ *
+ */
+
 public class DeckOfCardsMain {
     public static void main(String[] args) {
         System.out.println("Welcome to deck of cards game");
+        // create the object for PlayCards
+        PlayCards cardsPlay = new PlayCards();
+        // call to main method
+        cardsPlay.uniqueCards();
     }
 }
