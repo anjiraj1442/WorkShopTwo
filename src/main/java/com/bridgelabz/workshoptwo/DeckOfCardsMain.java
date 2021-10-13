8717b5f577b726d07f627993ee67413126319be7
anjirajardula <anjirajardula@gmail.com>
2021-10-13 08:58:40 +0530

    Added: Welcome to deck of cards game

---
+++ src/main/java/com/bridgelabz/workshoptwo/DeckOfCardsMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.workshoptwo;
+
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to deck of cards game");
+    }
+}
